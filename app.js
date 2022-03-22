0ee53d7121ad9d2ac8f0483d094a14cca0a71bb4
fatima <oud.tima@hotmail.com>
2022-03-22 22:16:20 +0100

    affichage et calcul des valeurs

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
 //Dom
 const touches = [...document.querySelectorAll('.bouton')];
 const listeKeyCode = touches.map(touche =>touche.dataset.key);
+const ecran =  document.querySelector('.ecran');
 
 document.addEventListener('keydown', (e) => {
-    const valeur = e.keyCode.toString(); 
-   
+    const valeur = e.keyCode.toString();
+    calculer(valeur)
 })
 
 document.addEventListener('click', (e) => {
     const valeur = e.target.dataset.key;
-    console.log(valeur, typeof valeur);
+    calculer(valeur)
 })
+
+const calculer = (valeur) => {
+    if(listeKeyCode.includes(valeur)) {
+        switch(valeur) {
+            case '8':
+                ecran.textContent = "";
+                break;
+            case '13':
+                const calcul = eval(ecran.textContent);
+                ecran.textContent = calcul; 
+                break;
+            default:
+                const indexKeyCode = listeKeyCode.indexOf(valeur);
+                const touche = touches[indexKeyCode];
+                ecran.textContent += touche.innerHTML;
+        }
+        
+    }
+}
